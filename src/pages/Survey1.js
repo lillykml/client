ad95714a08d3769ccf8bc03cd5c122d9be81d23d
Nicklas Schneider <nicklas@marken-schneider.de>
2023-10-21 16:17:21 -0400

    Added survey one

--- src/pages/Survey1.js
+++ src/pages/Survey1.js
@@ -1,11 +1,53 @@
-import Header from '../components/Header'
+import Header from '../components/Header';
+import React, { useState } from 'react';
+import './survey1.css';
+
+// MoodButton Component
+const MoodButton = ({ mood, setMood }) => {
+    return (
+      <button className="moodButton" onClick={() => setMood(mood)}>
+        <img src={`path_to_assets/${mood}.svg`} alt={mood} />
+      </button>
+    );
+  };
+  
+  // CustomButton Component
+  const CustomButton = ({ text, navigateTo }) => {
+    return (
+      <button className="customButton" onClick={navigateTo}>
+        {text}
+      </button>
+    );
+  };
 
 const Survey1 = () => {
+
+    const [mood, setMood] = useState(null);
+  
+    const navigateToNextQuestion = () => {
+      // Navigate to another question or main page based on the mood
+      if (mood) {
+        window.location.href = '/nextPage';
+      }
+    };
+
     return(
         <div>
             <Header />
-            <h2>Survey Page 1</h2>
-        </div>
+            <div className="moodSurvey">
+                <h2>Hi Courtney, how are you feeling today?</h2>
+                <div className="moodOptions">
+                    <MoodButton mood="Happy" setMood={setMood} />
+                    <MoodButton mood="Sad" setMood={setMood} />
+                    <MoodButton mood="Angry" setMood={setMood} />
+                    <MoodButton mood="Average" setMood={setMood} />
+                </div>
+                <div className="buttonOptions">
+                    <CustomButton text="Skip" navigateTo={navigateToNextQuestion} />
+                    <CustomButton text="Next" navigateTo={navigateToNextQuestion} />
+                </div>
+                </div>
+             </div>
     )
 }
 
