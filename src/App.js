74b6bd66146925b4a60c78bf209752d08c1318f1
lillykml <64362067+lillykml@users.noreply.github.com>
2023-10-21 15:03:21 -0400

    Added user component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import React, {useState, useEffect} from 'react'
+import User from './components/user'
 
 function App() {
 
   const [data, setData] = useState([{}])
+  const [user, setUser] = useState([{}])
 
   useEffect(() => {
     fetch("/members")     //fetches the /members route
@@ -16,8 +18,20 @@ function App() {
       })
   }, [])  // the brackets only run it once
 
+  useEffect(() => {
+    fetch("/user")     //fetches the /members route
+    .then(
+      res => res.json()  //we put the data in json
+    ).then(
+      user => {
+        setUser(user)      // we update our data variable from use State
+        console.log(user)
+      })
+  }, [])  // the brackets only run it once
+
   return (
     <div className="App">
+      <div>
       {(typeof data.members === 'undefined') ? (
         <p>Loading ...</p>
       ) : (
@@ -25,7 +39,8 @@ function App() {
           <p key={i}>{member}</p>
         ))
       )}
-
+      </div>
+      <User name={user.name}/>
     </div>
   );
 }
---
+++ src/components/user.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const User = (props) => {
+    return(
+        <div>
+            <p>
+                Hello {props.name}, how are you doing today?
+            </p>
+        </div>
+    )
+}
+
+export default User;
