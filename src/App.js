229d539b45ec2f4a870aef0148737e6894998744
lillykml <64362067+lillykml@users.noreply.github.com>
2023-10-21 11:13:11 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members")     //fetches the /members route
+    .then(
+      res => res.json()  //we put the data in json
+    ).then(
+      data => {
+        setData(data)       // we update our data variable from use State
+        console.log(data)
+      })
+  }, [])  // the brackets only run it once
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(typeof data.members === 'undefined') ? (
+        <p>Loading ...</p>
+      ) : (
+        data.members.map((member, i) => (
+          <p key={i}>{member}</p>
+        ))
+      )}
+
     </div>
   );
 }
