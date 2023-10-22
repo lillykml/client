05d0ef9dbc83c87f83d7d131f62ca7d7dd7291dc
Nicklas Schneider <nicklas@marken-schneider.de>
2023-10-21 20:37:06 -0400

    home screen 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import { useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
@@ -6,6 +5,9 @@ import './home.css';
 
 
 const Home = () => {
+
+    const [activeTab, setActiveTab] = useState('Mental'); // Default tab
+    const [selectedButton, setSelectedButton] = useState('General'); // Default button
 
     const navigate = useNavigate();
 
@@ -15,37 +17,62 @@ const Home = () => {
 
     return(
         <div>
-        <div className="main-screen-container">
-            <div className="header">
-                <p>October 21, 2023</p>
-                <h1>Welcome, Courtney</h1>
-            </div>
-            <div className="daily-summary">
-                <div className="well-being">
-                    <p>Well-being score: 54%</p>
-                    <p>You seem: STRESSED</p>
+            <div className="main-screen-container">
+                <div className="header">
+                    <p>October 21, 2023</p>
+                    <h1>Welcome, Courtney</h1>
                 </div>
-                <button onClick={navigateToInsights}>Recommendations</button>
-            </div>
-            <div className="brighten-day">
-                <h2>Let’s brighten up your day ☀️</h2>
+                <div className="summary-section">
+                    <div className="daily-summary">
+                        <p>Well-being score: 54%</p>
+                        <p>You seem: STRESSED</p>
+                        <button onClick={navigateToInsights}>Recommendations</button>
+                    </div>
+                    <div className="image-container">
+                        {/* Placeholder for Image */}
+                    </div>
+                </div>
+                <div className="brighten-day">
+                    <h2>Let’s brighten up your day</h2>
+                    <button>Click</button>
+                </div>
                 <div className="data-section">
-                    {/* Add data blocks here like steps, heart rate, etc. */}
+                    {['Steps', 'Heart rate', 'Stress', 'Mood'].map(header => (
+                        <div key={header} className="data-block">
+                            <h3>{header}</h3>
+                            <p>Placeholder</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="weekly-summary">
+                    <div className="tabs">
+                        <button
+                            style={activeTab === 'Mental' ? { backgroundColor: '#5FA7FF', color: 'white' } : {}}
+                            onClick={() => setActiveTab('Mental')}
+                        >
+                            Mental
+                        </button>
+                        <button
+                            style={activeTab === 'Physical' ? { backgroundColor: '#5FA7FF', color: 'white' } : {}}
+                            onClick={() => setActiveTab('Physical')}
+                        >
+                            Physical
+                        </button>
+                    </div>
+                    <div className="graph-container">
+                        {/* Placeholder for graph */}
+                        {['General', 'Mood', 'Stress'].map(buttonText => (
+                            <button
+                                key={buttonText}
+                                className={selectedButton === buttonText ? 'active' : ''}
+                                onClick={() => setSelectedButton(buttonText)}
+                            >
+                                {buttonText}
+                            </button>
+                        ))}
+                    </div>
                 </div>
             </div>
-            <div className="weekly-summary">
-                <div className="tabs">
-                    <button>Mental</button>
-                    <button>Physical</button>
-                </div>
-                {/* Placeholder for graph */}
-            </div>
-            <div className="footer-nav">
-                <button>Journal</button>
-                <button>Home</button>
-                <button>Insights</button>
-            </div>
-        </div>
             <Navbar current="home"/>
         </div>
     );
