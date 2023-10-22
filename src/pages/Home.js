6e190206023b8a0d319c2438e22c065ef40f038c
Nicklas Schneider <nicklas@marken-schneider.de>
2023-10-21 20:19:50 -0400

    survey2 redo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,54 @@
+import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import './home.css';
+
 
 const Home = () => {
 
+    const navigate = useNavigate();
+
+    const navigateToInsights = () => {
+        navigate('/survey1');
+    };
+
     return(
         <div>
-            <h2>Home Page</h2>
+        <div className="main-screen-container">
+            <div className="header">
+                <p>October 21, 2023</p>
+                <h1>Welcome, Courtney</h1>
+            </div>
+            <div className="daily-summary">
+                <div className="well-being">
+                    <p>Well-being score: 54%</p>
+                    <p>You seem: STRESSED</p>
+                </div>
+                <button onClick={navigateToInsights}>Recommendations</button>
+            </div>
+            <div className="brighten-day">
+                <h2>Let’s brighten up your day ☀️</h2>
+                <div className="data-section">
+                    {/* Add data blocks here like steps, heart rate, etc. */}
+                </div>
+            </div>
+            <div className="weekly-summary">
+                <div className="tabs">
+                    <button>Mental</button>
+                    <button>Physical</button>
+                </div>
+                {/* Placeholder for graph */}
+            </div>
+            <div className="footer-nav">
+                <button>Journal</button>
+                <button>Home</button>
+                <button>Insights</button>
+            </div>
+        </div>
             <Navbar current="home"/>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
--- src/pages/Survey2.js
+++ src/pages/Survey2.js
@@ -12,10 +12,52 @@ const CustomButton = ({ text, navigateTo }) => {
 };
 
 const Survey2 = () => {
-    return(
-        <div>
-            <h2>Survey Page 2</h2>
+  const [selectedReasons, setSelectedReasons] = useState([]);
+  const [otherReason, setOtherReason] = useState('');
+
+  const handleReasonClick = (reason) => {
+    if (selectedReasons.includes(reason)) {
+      setSelectedReasons(prevReasons => prevReasons.filter(r => r !== reason));
+    } else {
+      setSelectedReasons(prevReasons => [...prevReasons, reason]);
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const navigateToPreviousPage = () => {
+    navigate('/survey1');
+  };
+
+  const closeSurvey = () => {
+    navigate('/home');
+  };
+
+  return (
+    <div>
+      <div className="reasonsContainer">
+        <h2>Why are you feeling that way?</h2>
+        <div className="reasonsButtons">
+          {["Stress at work/school", "Got into an argument", "Achieved something", "Spent time outdoors", "Didn’t sleep well", "Celebrated something"].map(reason => (
+            <button
+              key={reason}
+              className={selectedReasons.includes(reason) ? 'selected' : ''}
+              onClick={() => handleReasonClick(reason)}
+            >
+              {reason}
+            </button>
+          ))}
         </div>
+        <div className="otherReason">
+          <label>Other reason</label>
+          <input type="text" value={otherReason} onChange={e => setOtherReason(e.target.value)} placeholder="Type here..." />
+        </div>
+        <div className="navigationButtons">
+          <CustomButton text="Back" navigateTo={navigateToPreviousPage} />
+          <CustomButton text="Finish" navigateTo={closeSurvey} />
+        </div>
+      </div>
+    </div>
   );
 };
 
