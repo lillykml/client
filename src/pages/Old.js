41bdaf86cec1de9c1f9afbbf85339ccb971b7550
lillykml <64362067+lillykml@users.noreply.github.com>
2023-10-21 23:14:52 -0400

    Fixed fetching errors & test

--- src/pages/Old.js
+++ src/pages/Old.js
@@ -1,33 +1,22 @@
 import React, {useState, useEffect} from 'react'
-import User from '../components/user'
 
 function Old() {
 
-  const [data, setData] = useState([{}])
-  const [user, setUser] = useState([{}])
+  const [steps, setSteps] = useState([{}])
 
   useEffect(() => {
-    fetch("/members")     //fetches the /members route
+    fetch("/current_steps") 
+    .then(res => res.json())
     .then(
-      res => res.json()  //we put the data in json
-    ).then(
       data => {
-        setData(data)       // we update our data variable from use State
+        setSteps(data)
         console.log(data)
       })
-  }, [])  // the brackets only run it once
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-      {(typeof data.members === 'undefined') ? (
-        <p>Loading ...</p>
-      ) : (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
-      )}
-      </div>
+    <div>
+      <h1>Current Steps are {steps.current_steps}</h1>
     </div>
   );
 }
